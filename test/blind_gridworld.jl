7921fb5753f2add56e47f41a813ab45eb654b017
MaximeBouton <maxim.bouton@gmail.com>
2018-11-27 17:45:24 -0800

    add reachability tests

--- test/blind_gridworld.jl
+++ test/blind_gridworld.jl
@@ -1,7 +1,15 @@
-# Blind GridWorld
-# there is one exit the robot can only observe 1 if it is at the exit 
-# it observes 0 otherwise
+#=
+Blind GridWorld
+two observation models are implemented (one is commented out)
+Model 1:
+The robot is completely blind and observe false all the time 
+Model 2:
+The robot has a noisy measurement of its position, it modeled
+by a uniform distribution around the neighbor of the current state (diagonal included)
+=#
+
 using StaticArrays
+using LinearAlgebra
 using Random
 using POMDPs
 using POMDPModels
@@ -11,12 +19,10 @@ using ColorSchemes
 using Parameters
 using BeliefUpdaters
 
-const GWPos = POMDPModels.GWPos
-
-@with_kw struct BlindGridWorld <: POMDP{GWPos, Symbol, Bool}
+@with_kw struct BlindGridWorld <: POMDP{GWPos, Symbol, GWPos} #Bool
     size::Tuple{Int64, Int64} = (10,10)
     exit::GWPos = GWPos(10,1)
-    simple_gw::SimpleGridWorld = SimpleGridWorld(size=size, rewards=Dict(exit=>1.0), terminate_from=Set())
+    simple_gw::SimpleGridWorld = SimpleGridWorld(size=size, rewards=Dict(exit=>1.0), terminate_from=Set([exit]))
 end
 
 ## States 
@@ -24,7 +30,7 @@ end
 POMDPs.states(pomdp::BlindGridWorld) = states(pomdp.simple_gw)
 POMDPs.n_states(pomdp::BlindGridWorld) = n_states(pomdp.simple_gw)
 POMDPs.stateindex(pomdp::BlindGridWorld, s::AbstractVector{Int}) = stateindex(pomdp.simple_gw, s)
-POMDPs.initialstate(pomdp::BlindGridWorld, rng::AbstractRNG) = initialstate(pomdp.simple_gw)
+POMDPs.initialstate(pomdp::BlindGridWorld, rng::AbstractRNG) = initialstate(pomdp.simple_gw, rng::AbstractRNG)
 POMDPs.initialstate_distribution(pomdp::BlindGridWorld) = uniform_belief(pomdp)
 
 ## Actions 
@@ -35,27 +41,53 @@ POMDPs.actionindex(pomdp::BlindGridWorld, a::Symbol) = actionindex(pomdp.simple_
 
 ## Transition
 
-POMDPs.isterminal(m::BlindGridWorld, s::AbstractVector{Int}) = s == m.exit
+POMDPs.isterminal(m::BlindGridWorld, s::AbstractVector{Int}) = (s == m.exit || s == GWPos(-1,-1))
 POMDPs.transition(pomdp::BlindGridWorld, s::AbstractVector{Int}, a::Symbol) = transition(pomdp.simple_gw, s, a)
 
 ## Observation 
 
-POMDPs.observations(pomdp::BlindGridWorld) = (false, true)
-POMDPs.obsindex(pomdp::BlindGridWorld, s::Bool) = Int(s) + 1
+# Model 1
+
+# POMDPs.observations(pomdp::BlindGridWorld) = (false, true)
+# POMDPs.obsindex(pomdp::BlindGridWorld, s::Bool) = Int(s) + 1
+# POMDPs.n_observations(pomdp::BlindGridWorld) = 2
+# POMDPs.generate_o(pomdp::BlindGridWorld, s, rng) = false
+
+# function POMDPs.observation(pomdp::BlindGridWorld, a::Symbol, sp::AbstractVector{Int})
+#     if sp == pomdp.exit 
+#         return BoolDistribution(0.0)
+#     else
+#         return BoolDistribution(0.0)
+#     end
+# end
+
+# Model 2
+
+POMDPs.observations(pomdp::BlindGridWorld) = states(pomdp)
+POMDPs.obsindex(pomdp::BlindGridWorld, o) = stateindex(pomdp, o)
+POMDPs.n_observations(pomdp::BlindGridWorld) = n_states(pomdp)
+POMDPs.generate_o(pomdp::BlindGridWorld, s, rng) = rand(rng, observation(pomdp, first(actions(pomdp)), s))
+
+const NEIGHBORS_DIRECTIONS = Set([GWPos(0,1), GWPos(0,-1), GWPos(-1,0), GWPos(1,0), 
+                                  GWPos(1,1), GWPos(-1,1), GWPos(1,-1), GWPos(-1,-1)])
 
 function POMDPs.observation(pomdp::BlindGridWorld, a::Symbol, sp::AbstractVector{Int})
-    if sp == pomdp.exit 
-        return BoolDistribution(0.0)
-    else
-        return BoolDistribution(0.0)
-    end
-end
 
-function POMDPModels.tocolor(r::Float64)
-    minr = -1.0
-    maxr = 1.0
-    frac = (r-minr)/(maxr-minr)
-    return get(ColorSchemes.redgreensplit, frac)
+    neighbors = MVector{length(NEIGHBORS_DIRECTIONS) + 1, GWPos}(undef)
+    neighbors[1] = sp
+
+    # probs = MVector{n_actions(mdp)+1, Float64}() 
+    probs = @MVector(ones(9)) 
+    for (i, dir) in enumerate(NEIGHBORS_DIRECTIONS)
+        neighbors[i+1] = sp + dir
+
+        if !POMDPModels.inbounds(pomdp.simple_gw, neighbors[i+1]) # hit an edge and come back
+            probs[i+1] = 0.
+        end
+    end
+    normalize!(probs, 1)
+
+    return SparseCat(neighbors, probs)
 end
 
 ## Reward 
@@ -64,35 +96,114 @@ POMDPs.reward(pomdp::BlindGridWorld, s, a, sp) = reward(pomdp.simple_gw, s)
 POMDPs.reward(pomdp::BlindGridWorld, s, a) = reward(pomdp.simple_gw, s)
 POMDPs.discount(pomdp::BlindGridWorld) = 1.0
 
+## distributions 
+POMDPs.initialstate_distribution(pomdp::BlindGridWorld) = uniform_belief(pomdp)
 
-## Render 
+## helpers
+
+function deterministic_belief(pomdp, s)
+    b = zeros(n_states(pomdp))
+    si = stateindex(pomdp, s)
+    b[si] = 1.0
+    return DiscreteBelief(pomdp, b)
+end
+
+## Rendering 
+
+function POMDPModels.tocolor(r::Float64, minr=0., maxr=1.0)
+    frac = (r-minr)/(maxr-minr)
+    return get(ColorSchemes.redgreensplit, frac)
+end
 
 function POMDPModels.render(mdp::SimpleGridWorld, step::Union{NamedTuple,Dict};
-                color = s->reward(mdp, s)
+                valuecolor = s -> reward(mdp, s),
+                value = nothing,
+                action = nothing,
+                landmark = nothing,
+                minr = 0.,
+                maxr = 1.0,
                )
 
     nx, ny = mdp.size
     cells = []
     vals = []
+    acts = []
+    landmarks = []
     for x in 1:nx, y in 1:ny
-        clr = POMDPModels.tocolor(color(GWPos(x,y)))
+        clr = valuecolor != nothing ? POMDPModels.tocolor(valuecolor(GWPos(x,y)), minr, maxr) : RGB(1.0,1.0,1.0)
         ctx = POMDPModels.cell_ctx((x,y), mdp.size)
         cell = compose(ctx, rectangle(), fill(clr))
-        val = compose(ctx, text(0.5,0.5, round(color(GWPos(x,y)), digits=3), hcenter, vcenter))
-        push!(vals, val)
+        if value != nothing
+            val = compose(ctx, text(0.5,0.5, round(value(GWPos(x,y)), digits=3), hcenter, vcenter))
+            push!(vals, val)
+        end
+        
+        if action != nothing 
+            act = actionarrow(ctx, action(GWPos(x,y)))
+            push!(acts, act)
+        end
+
+        if landmark != nothing 
+            if landmark(GWPos(x,y))
+                push!(landmarks, compose(ctx, circle(0.5, 0.5, 0.3), fill("purple")))
+            end
+        end
+
         push!(cells, cell)
     end
     vals = compose(context(), vals...)
+    acts = compose(context(), acts...)
+    landmarks = compose(context(), landmarks...)
     grid = compose(context(), linewidth(1Compose.mm), stroke("gray"), cells...)
     outline = compose(context(), linewidth(1Compose.mm), rectangle())
 
     if haskey(step, :s)
         agent_ctx = POMDPModels.cell_ctx(step[:s], mdp.size)
         agent = compose(agent_ctx, circle(0.5, 0.5, 0.4), fill("orange"))
+        if haskey(step, :a)
+            agent = compose(agent, actionarrow(agent_ctx, step[:a]))
+        end 
     else
         agent = nothing
     end
+
+
     
     sz = min(w,h)
-    return compose(context((w-sz)/2, (h-sz)/2, sz, sz), agent, vals, grid)
+    gw_ctx = context((w-sz)/2, (h-sz)/2, sz, sz)
+    if value != nothing
+        gw_ctx = compose(gw_ctx, vals)
+    end
+    if action != nothing 
+        gw_ctx = compose(gw_ctx, acts)
+    end
+    if landmark != nothing 
+        gw_ctx = compose(gw_ctx, landmarks)
+    end
+    return compose(gw_ctx, agent, grid)
+end
+
+function actionarrow(ctx, act::Symbol)
+    if act == :up 
+        return compose(ctx, line([(0.5,0.5), (0.5, 0.35)]), stroke(["gray"]), arrow())
+    elseif act == :down
+        return compose(ctx, line([(0.5,0.5), (0.5, 0.65)]), stroke(["gray"]), arrow())
+                        
+    elseif act == :right
+        return compose(ctx, line([(0.5,0.5), (0.65, 0.5)]), stroke(["gray"]), arrow())
+                      
+    elseif act == :left 
+        return compose(ctx, line([(0.5,0.5), (0.35, 0.5)]), stroke(["gray"]), arrow())            
+    end
+end
+
+function highlight_goodstates(ctx, mdp::SimpleGridWorld, states::Vector{GWPos})
+    goodstates = []
+    for s in states
+        x, y = s
+        cctx = POMDPModels.cell_ctx((x,y), mdp.size)
+        push!(goodstates, compose(cctx, circle(), fill("purple")))
+    end
+    goodstates = compose(context(), goodstates...)
+    compose(goodstates, ctx)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,92 +1,33 @@
-using Revise
 using MDPModelChecking
 using POMDPs
-using POMDPModelTools
 using POMDPModels
-using POMDPTesting
+using DiscreteValueIteration
+using QMDP
 using Test
 
-@testset "automata parsing" begin
-    include("test_parsing.jl")
+@testset "MDP reachability" begin 
+    mdp = SimpleGridWorld(size=(10,10), terminate_from=Set([GWPos(9,3), GWPos(4,3)]), tprob=0.7)
+    reach = Set([GWPos(10,1)])
+    avoid = mdp.terminate_from
+    solver = ReachabilitySolver(reach, avoid, ValueIterationSolver())
+    policy = solve(solver, mdp)
+    @test value(policy, GWPos(10,1)) == 1.
+    @test value(policy, GWPos(4,3)) == 0.
+    @test value(policy, GWPos(9,3)) == 0.
+    @test action(policy, GWPos(9,1)) == :right
 end
 
-@testset "product MDP" begin 
-    include("test_product.jl")
+@testset "POMDP reachability" begin 
+    include("blind_gridworld.jl")
+    pomdp = BlindGridWorld(size=(10,10), 
+                           exit=GWPos(10,1), 
+                           simple_gw=SimpleGridWorld(size=(10,10), terminate_from=Set([GWPos(9,3), GWPos(4,3)]), tprob=0.7))
+    reach = Set([GWPos(10,1)])
+    avoid = pomdp.simple_gw.terminate_from
+    solver = ReachabilitySolver(reach, avoid, QMDPSolver())
+    policy = solve(solver, pomdp)
+    @test value(policy, deterministic_belief(pomdp, GWPos(10,1))) == 0. # terminal state in QMDP value function is 0.
+    @test value(policy, deterministic_belief(pomdp, GWPos(4,3))) == 0.
+    @test value(policy, deterministic_belief(pomdp, GWPos(9,3))) == 0.
+    @test action(policy, deterministic_belief(pomdp, GWPos(9,1))) == :right
 end
-
-# @testset "maximal end components" begin 
-#     include("test_mec.jl")
-# end
-
-function label_grid_world(mdp::GridWorld)
-    good_states = mdp.reward_states[mdp.reward_values .> 0.]
-    bad_states = mdp.reward_states[mdp.reward_values .< 0.]
-    labeling = Dict{statetype(mdp), Vector{String}}()
-    for s in good_states
-        labeling[s] = ["good"]
-    end
-    for s in bad_states
-        labeling[s] = ["bad"]
-    end
-    labeling[GridWorldState(0, 0, true)] = ["good", "term"]
-    return labeling
-end
-
-
-function POMDPs.initialstate_distribution(mdp::GridWorld)
-    return SparseCat(states(mdp), 1/n_states(mdp)*ones(n_states(mdp)))
-end
-
-rng = MersenneTwister(1)
-
-mdp = GridWorld()
-labeling = label_grid_world(mdp)
-property = "Pmax=? [ (!\"bad\") U \"good\"]"  # follow stormpy syntax
-
-# test model writing
-write_mdp_transition(mdp)
-write_mdp_labels(mdp, labeling)
-
-# test model parsing
-model = parse_mdp_model(mdp, labeling)
-model = parse_mdp_model("mdp.tra", "mdp.lab")
-
-# test model checking 
-result = model_checking(mdp, labeling, property, 
-    transition_file_name="gw.tra",
-    labels_file_name="gw.lab")
-
-# test overwrite
-println("There should be a warning between here: ")
-result = model_checking(mdp, labeling, property, 
-    transition_file_name="gw.tra",
-    labels_file_name="gw.lab",
-    overwrite = true)
-println("And here. ")
-
-# test scheduler extraction 
-scheduler = Scheduler(mdp, result)
-
-# test scheduler actions 
-s = rand(rng, states(mdp))
-a = action(scheduler, s)
-
-# test proba extraction 
-P = get_proba(mdp, result)
-P_sa = get_state_action_proba(mdp, P)
-
-# test policy creation from model checking result
-vec_policy = VectorPolicy(mdp, result)
-a = action(vec_policy, s)
-
-# test masking
-threshold = 0.99
-safety_mask = SafetyMask(mdp, result, threshold)
-safe_actions(safety_mask, s)
-
-# test masked policy 
-masked_eg = MaskedEpsGreedyPolicy(mdp, 0.7, safety_mask, MersenneTwister(1))
-masked_v = MaskedValuePolicy(ValuePolicy(mdp), safety_mask)
-
-p0 = initial_probability(mdp, result)
-@test isapprox(p0, 0.964, atol=0.001)
